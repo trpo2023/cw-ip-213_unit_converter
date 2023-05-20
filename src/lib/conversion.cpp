9148372cedb769cbcfe97c1707b7af030f8c9b1b
Yuriy Petrushenko <akeno7@mail.ru>
2023-05-20 19:50:48 +0700

    Minor optimization, #14

--- src/lib/conversion.cpp
+++ src/lib/conversion.cpp
@@ -4,13 +4,18 @@
 #define MAX_LENGTH 25
 
 double
-Convert(FILE* units,
-        char* HAVEunit,
+Convert(char* HAVEunit,
         int uHLength,
         char* WANTunit,
         int uWLength,
         double result)
 {
+    FILE* units = fopen("UnitsData", "r");
+    if (units == NULL) {
+        printf("Error: incorrect file path\n");
+        return 0;
+    }
+
     char str[30];
     char HAVEunitName[100];
     char WANTunitName[100];
@@ -23,28 +28,26 @@ Convert(FILE* units,
 
         if (!(strncmp(HAVEunit, str, struLength)) && struLength == uHLength) {
             result *= strtod(strchr(str, ' '), NULL);
-            while (fgets(str, 30, units))
-                if (str[0] == '#') {
-                    strcpy(HAVEunitName, str);
-                    i = 0;
-                    while (HAVEunitName[i] != '\n')
-                        i++;
-                    HAVEunitName[i] = '\0';
-                    break;
-                }
+            if (str[0] == '#') {
+                strcpy(HAVEunitName, str);
+                i = 0;
+                while (HAVEunitName[i] != '\n')
+                    i++;
+                HAVEunitName[i] = '\0';
+                break;
+            }
         }
 
         if (!(strncmp(WANTunit, str, struLength)) && struLength == uWLength) {
             result /= strtod(strchr(str, ' '), NULL);
-            while (fgets(str, 30, units))
-                if (str[0] == '#') {
-                    strcpy(WANTunitName, str);
-                    i = 0;
-                    while (WANTunitName[i] != '\n')
-                        i++;
-                    WANTunitName[i] = '\0';
-                    break;
-                }
+            if (str[0] == '#') {
+                strcpy(WANTunitName, str);
+                i = 0;
+                while (WANTunitName[i] != '\n')
+                    i++;
+                WANTunitName[i] = '\0';
+                break;
+            }
         }
     }
     fseek(units, 0, SEEK_END);
